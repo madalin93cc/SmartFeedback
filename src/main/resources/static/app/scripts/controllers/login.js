092bf96dd4b30e5872b1d520007f5bc67a8d5ba7
madalin93cc <centr0PEN%^&>
2015-11-22 21:37:15 +0200

    login page

--- src/main/resources/static/app/scripts/controllers/login.js
+++ src/main/resources/static/app/scripts/controllers/login.js
@@ -8,6 +8,23 @@
  * Controller of the smartFeedbackApp
  */
 angular.module('smartFeedbackApp')
-  .controller('LoginCtrl', ['$scope', function ($scope) {
+  .controller('LoginCtrl', ['$rootScope', '$scope', '$location', function ($rootScope, $scope, $location) {
+
+    $scope.login = function () {
+      $rootScope.isAuthenticated = true;
+      $location.path("#/");
+    }
+
+    $scope.logout = function () {
+      $rootScope.isAuthenticated = false;
+    }
 
   }]);
+
+app.run(['$rootScope', '$location', function ($rootScope, $location) {
+  $rootScope.$on('$routeChangeStart', function (event) {
+    if (!$rootScope.isAuthenticated){
+      $location.path("/login");
+    }
+  });
+}]);
