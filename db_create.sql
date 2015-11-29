4b3e0d5afa31e181d5f8584090b0ada8843a773e
george1604 <george.musat93@gmail.com>
2015-11-29 17:59:50 +0200

    script db

--- db_create.sql
+++ db_create.sql
@@ -13,13 +13,13 @@
 -- Dumping structure for table smartfeedback.activitate
 DROP TABLE IF EXISTS `activitate`;
 CREATE TABLE IF NOT EXISTS `activitate` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `an` int(11) NOT NULL,
   `nume` varchar(255) NOT NULL,
   `code` varchar(50) NOT NULL,
   `id_tip_activitate` int(11) NOT NULL,
   `id_serie` int(11) NOT NULL,
-  `id_grupa` int(11) NOT NULL,
+  `id_grupa` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_activitate_tip_activitate` (`id_tip_activitate`),
   KEY `FK_activitate_serie` (`id_serie`),
@@ -32,12 +32,34 @@ CREATE TABLE IF NOT EXISTS `activitate` (
 -- Data exporting was unselected.
 
 
+-- Dumping structure for table smartfeedback.comentariu
+DROP TABLE IF EXISTS `comentariu`;
+CREATE TABLE IF NOT EXISTS `comentariu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `text` text NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `id_parinte` int(11) DEFAULT NULL,
+  `id_utilizator` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_comentariu_comentariu` (`id_parinte`),
+  KEY `FK_comentariu_utilizator` (`id_utilizator`),
+  CONSTRAINT `FK_comentariu_comentariu` FOREIGN KEY (`id_parinte`) REFERENCES `comentariu` (`id`),
+  CONSTRAINT `FK_comentariu_utilizator` FOREIGN KEY (`id_utilizator`) REFERENCES `utilizator` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+
 -- Dumping structure for table smartfeedback.feedback
 DROP TABLE IF EXISTS `feedback`;
 CREATE TABLE IF NOT EXISTS `feedback` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nume` varchar(255) NOT NULL,
   `id_activitate` int(11) NOT NULL,
+  `status` int(11) NOT NULL COMMENT '1-activ; 2-inactiv',
+  `from_date` datetime DEFAULT NULL,
+  `to_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_feedback_activitate` (`id_activitate`),
   CONSTRAINT `FK_feedback_activitate` FOREIGN KEY (`id_activitate`) REFERENCES `activitate` (`id`)
@@ -46,13 +68,61 @@ CREATE TABLE IF NOT EXISTS `feedback` (
 -- Data exporting was unselected.
 
 
+-- Dumping structure for table smartfeedback.feedback_intrebari
+DROP TABLE IF EXISTS `feedback_intrebari`;
+CREATE TABLE IF NOT EXISTS `feedback_intrebari` (
+  `id_feedback` int(11) NOT NULL,
+  `id_intrebare` int(11) NOT NULL,
+  PRIMARY KEY (`id_feedback`,`id_intrebare`),
+  KEY `FK_feedback_intrebari_intrebari` (`id_intrebare`),
+  CONSTRAINT `FK__feedback` FOREIGN KEY (`id_feedback`) REFERENCES `feedback` (`id`),
+  CONSTRAINT `FK_feedback_intrebari_intrebari` FOREIGN KEY (`id_intrebare`) REFERENCES `intrebari` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+
 -- Dumping structure for table smartfeedback.grupa
 DROP TABLE IF EXISTS `grupa`;
 CREATE TABLE IF NOT EXISTS `grupa` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nume` varchar(50) DEFAULT NULL,
   `an` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table smartfeedback.intrebari
+DROP TABLE IF EXISTS `intrebari`;
+CREATE TABLE IF NOT EXISTS `intrebari` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `intrebare` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table smartfeedback.mesaj
+DROP TABLE IF EXISTS `mesaj`;
+CREATE TABLE IF NOT EXISTS `mesaj` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user_send` int(11) NOT NULL,
+  `id_user_recv` int(11) NOT NULL,
+  `id_parinte` int(11) DEFAULT NULL,
+  `seen` int(11) DEFAULT NULL COMMENT 'null-nu; 0-nu; 1-da',
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `text` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_mesaj_utilizator` (`id_user_send`),
+  KEY `FK_mesaj_utilizator_2` (`id_user_recv`),
+  KEY `FK_mesaj_mesaj` (`id_parinte`),
+  CONSTRAINT `FK_mesaj_mesaj` FOREIGN KEY (`id_parinte`) REFERENCES `mesaj` (`id`),
+  CONSTRAINT `FK_mesaj_utilizator` FOREIGN KEY (`id_user_send`) REFERENCES `utilizator` (`id`),
+  CONSTRAINT `FK_mesaj_utilizator_2` FOREIGN KEY (`id_user_recv`) REFERENCES `utilizator` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
@@ -78,6 +148,23 @@ CREATE TABLE IF NOT EXISTS `profesor_activitate` (
   CONSTRAINT `FK_profesor_activitate_activitate` FOREIGN KEY (`id_activitate`) REFERENCES `activitate` (`id`),
   CONSTRAINT `FK_profesor_activitate_profesor` FOREIGN KEY (`id_profesor`) REFERENCES `profesor` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='profesor_activitate';
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table smartfeedback.raspuns_intrebare
+DROP TABLE IF EXISTS `raspuns_intrebare`;
+CREATE TABLE IF NOT EXISTS `raspuns_intrebare` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_feedback` int(11) NOT NULL,
+  `id_student` int(11) NOT NULL,
+  `raspuns` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK__feedback_raspuns` (`id_feedback`),
+  KEY `FK__student_raspuns` (`id_student`),
+  CONSTRAINT `FK__feedback_raspuns` FOREIGN KEY (`id_feedback`) REFERENCES `feedback` (`id`),
+  CONSTRAINT `FK__student_raspuns` FOREIGN KEY (`id_student`) REFERENCES `student` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 
@@ -132,9 +219,9 @@ CREATE TABLE IF NOT EXISTS `student_activitate` (
   `id_student` int(11) NOT NULL,
   `id_activitate` int(11) NOT NULL,
   PRIMARY KEY (`id_student`,`id_activitate`),
-  KEY `FK__activitate` (`id_activitate`),
-  CONSTRAINT `FK__activitate` FOREIGN KEY (`id_activitate`) REFERENCES `activitate` (`id`),
-  CONSTRAINT `FK__student` FOREIGN KEY (`id_student`) REFERENCES `student` (`id`)
+  KEY `FK_student_activitate_activitate` (`id_activitate`),
+  CONSTRAINT `FK__student` FOREIGN KEY (`id_student`) REFERENCES `student` (`id`),
+  CONSTRAINT `FK_student_activitate_activitate` FOREIGN KEY (`id_activitate`) REFERENCES `activitate` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
