b4a1a73bbaa2525f4e71bea2a9d2245e4a7ffbfb
madalin93cc <centr0PEN%^&>
2015-11-29 23:27:36 +0200

    logged user

--- src/main/java/ro/upb/smartfeedback/SmartFeedback.java
+++ src/main/java/ro/upb/smartfeedback/SmartFeedback.java
@@ -2,10 +2,11 @@ package ro.upb.smartfeedback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ro.upb.smartfeedback.entity.User;
 
 @SpringBootApplication
 public class SmartFeedback {
-
+    public static User loggedUser;
     public static void main(String[] args) {
         SpringApplication.run(SmartFeedback.class, args);
     }
