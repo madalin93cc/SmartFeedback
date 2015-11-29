c6cbaeb5f841791060c6dec527bf8cb153a12001
madalin93cc <centr0PEN%^&>
2015-11-30 00:12:38 +0200

    schimbare

--- src/main/java/ro/upb/smartfeedback/rest/LoginController.java
+++ src/main/java/ro/upb/smartfeedback/rest/LoginController.java
@@ -1,10 +1,7 @@
 package ro.upb.smartfeedback.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ro.upb.smartfeedback.service.LoginService;
 import ro.upb.smartfeedback.service.impl.LoginServiceImpl;
 import ro.upb.smartfeedback.utils.RequestMappings;
@@ -19,7 +16,7 @@ public class LoginController {
     LoginService loginService;
 
     @RequestMapping(value = RequestMappings.GET_LOGIN_PARAMS, method = RequestMethod.GET, produces = "application/json")
-    public Boolean getSuccessLogin(@RequestParam("username") String username, @RequestParam("password") String password) {
+    public Boolean getSuccessLogin(@PathVariable("username") String username, @PathVariable("password") String password) {
         return loginService.login(username, password);
     }
 
