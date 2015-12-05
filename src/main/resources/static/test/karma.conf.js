33da7f0b22e4c823ad3cc35a1eaa025a4b542db7
madalin93cc <centr0PEN%^&>
2015-12-06 00:18:11 +0200

    canvas js

--- src/main/resources/static/test/karma.conf.js
+++ src/main/resources/static/test/karma.conf.js
@@ -43,7 +43,6 @@ module.exports = function(config) {
       'bower_components/datatables-responsive/js/dataTables.responsive.js',
       'bower_components/flot.tooltip/js/jquery.flot.tooltip.js',
       'bower_components/angularjs-slider/dist/rzslider.js',
-      'bower_components/Graphs/src/jquery.Graphs.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
