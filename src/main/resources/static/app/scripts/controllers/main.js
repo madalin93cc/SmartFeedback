a0face8ad10619c9e794b1917184f8e21d0301a5
madalin93cc <centr0PEN%^&>
2015-12-10 01:30:35 +0200

    fixes

--- src/main/resources/static/app/scripts/controllers/main.js
+++ src/main/resources/static/app/scripts/controllers/main.js
@@ -9,9 +9,5 @@
  */
 angular.module('smartFeedbackApp')
   .controller('MainCtrl', ['$scope', function ($scope) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
   }]);
