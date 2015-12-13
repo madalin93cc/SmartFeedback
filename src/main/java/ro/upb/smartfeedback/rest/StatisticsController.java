1803be30e9ac1129bc22c7eb4e55fcf10727cf71
madalin93cc <colezea.madalin@yahoo.com>
2015-12-13 18:20:38 +0200

    statistics controller

---
+++ src/main/java/ro/upb/smartfeedback/rest/StatisticsController.java
@@ -0,0 +1,19 @@
+package ro.upb.smartfeedback.rest;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import ro.upb.smartfeedback.utils.RequestMappings;
+
+/**
+ * Created by colez on 13/12/2015.
+ */
+@RestController
+public class StatisticsController {
+
+    @RequestMapping(value = RequestMappings.GET_MAIN_PAGE_OVERVIEW, method = RequestMethod.GET, produces = "application/json")
+    public void getOverview(@PathVariable("idUser") Long isUser){
+
+    }
+}
